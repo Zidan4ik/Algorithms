e5e4af6a7f4bb42dbc1a6a6d336ab7782770857e
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-01-09 03:02:26 +0200

    Algorithms/Sorting algorithms (add bubble sort) #1

--- src/algorithm_sortingAlgorithms/bubble_sort/Bubble.java
+++ src/algorithm_sortingAlgorithms/bubble_sort/Bubble.java
@@ -3,21 +3,40 @@ package algorithm_sortingAlgorithms.bubble_sort;
 import java.util.Arrays;
 
 public class Bubble {
-    public static void main(String[] args) {
-        int [] array = new int[]{5,2,1,8,0,-15,12,62,1};
-        boolean flag = true;
+    static void bubbleSort(int array[]) {
+        int size = array.length;
 
-        while(flag){
-            flag=false;
-            for (int i = 0; i < array.length-1; i++) {
-                if(array[i]>array[i+1]){
-                    int num = array[i];
-                    array[i] = array[i+1];
-                    array[i+1]=num;
-                    flag=true;
+        // loop to access each array element
+        for (int i = 0; i < (size - 1); i++) {
+
+            boolean flag = false; // check if swapping occurs
+
+            for (int j = 0; j < (size - i - 1); j++) { // loop to compare elements
+
+
+                if (array[j] > array[j + 1]) { //comparing between elements
+
+                    // process trading on position between elements
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    flag = true;
                 }
             }
+            if (!flag) // check flag if there was no change elements on indexes
+                break;
+
         }
-        System.out.println(Arrays.toString(array));
+    }
+
+    public static void main(String[] args) {
+        int[] data = {-2, 45, 0, 11, -9};
+
+        Bubble.bubbleSort(data);
+
+        System.out.println("Sorted Array in Ascending Order:");
+        System.out.println(Arrays.toString(data));
     }
 }
+
